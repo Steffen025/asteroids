88b5ddf96e92d4507fcdcf012f6dc1610fd21f19
Steffen025 <st.zellmer@gmail.com>
2025-07-15 17:50:07 +0200

    Ch.2 L.3 FPS

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ def main():
     
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    Clock = pygame.time.Clock()
+    dt = 0
     
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -15,6 +17,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))  # Fill the screen with black
+        Clock.tick(60) # Limit the frame rate to 60 FPS
+        dt = Clock.tick(60) / 1000.0
+        # Here you would typically update game state and draw objects
         pygame.display.flip()
         
     
